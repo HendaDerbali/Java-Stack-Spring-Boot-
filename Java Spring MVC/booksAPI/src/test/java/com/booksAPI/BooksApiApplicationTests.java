0a735c3587afda26f38e68d9c698abde6a355661
Herbali Henda <derbalihenda93@gmail.com>
2024-02-11 02:12:21 +0100

    booksAPIs

---
+++ Java Spring MVC/booksAPI/src/test/java/com/booksAPI/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

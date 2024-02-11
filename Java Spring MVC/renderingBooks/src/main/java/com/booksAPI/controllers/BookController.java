3018cde0514b156c39704ff29aefe895d166fcc9
Herbali Henda <derbalihenda93@gmail.com>
2024-02-11 19:53:08 +0100

    Rendering All Books

--- Java Spring MVC/renderingBooks/src/main/java/com/booksAPI/controllers/BookController.java
+++ Java Spring MVC/renderingBooks/src/main/java/com/booksAPI/controllers/BookController.java
@@ -1,5 +1,7 @@
 package com.booksAPI.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,22 +18,35 @@ public class BookController {
 	
 	//First Step Usually add this to link this controller with services :
 	@Autowired
-	BookService bookService;@RequestMapping("")
-	
+	BookService bookService;
 	
 	//Second Steps : Routes
+	
+	@RequestMapping("")
 	public String home() {
 		return "redirect:/books";
 	}
 
 	
+	// ?Get All
+	@RequestMapping("/books")
+	public String index(Model model) {
+		List<Book> books = bookService.allBooks();
+		model.addAttribute("books", books);
+		System.out.println(books); // just to show my console
+
+		return "index.jsp";
+	}
+	
+	
+	// ? Get One By id
 	@RequestMapping("/books/{id}")
 		// Model bech yhez data: 
 	public String showOne(Model model,@PathVariable("id") Long id) {
 		// Fetch the book by its ID and add it to the model
 		Book book = bookService.findBook(id);
 		model.addAttribute("book", book);
-		System.out.println(book);
+		System.out.println(book); // just to show my console
 		
 		return "show.jsp";
 	}
---
+++ Java Spring MVC/renderingBooks/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!-- New line below to use the JSP Standard Tag Library -->
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<!-- for Bootstrap CSS -->
+	<link rel="stylesheet" 	href="/webjars/bootstrap/css/bootstrap.min.css" />
+	<!-- YOUR own local CSS -->
+	<link rel="stylesheet" href="/css/main.css"/>
+	<!-- For any Bootstrap that uses JS -->
+	<script src="/webjars/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<h1>All Books</h1>
+	<table class ="table">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Title</th>
+				<th>Language</th>
+				<th>Number of Pages</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- loop over all the books to show the details as in the wireframe! -->
+			<c:forEach var="book" items="${books}">
+				<tr>
+					<td>${book.id}</td>
+					<td><a href="/books/${book.id}">${book.title}</a></td>
+					<td>${book.language}</td>
+					<td>${book.numberOfPages}</td>
+				</tr>
+			</c:forEach>
+
+		</tbody>
+	</table>
+</body>
+</html>
